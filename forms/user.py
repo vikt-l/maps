064a543b758b6efcfe6243ef296f59e5c35bb189
Fedorov-Dima <fedorov.2006@yandex.ru>
2023-04-03 16:53:01 +0300

    Экран приветствия, вход и регистрация без дизайна

--- forms/user.py
+++ forms/user.py
@@ -8,6 +8,10 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     name = StringField('Имя пользователя', validators=[DataRequired()])
+    surname = StringField('Фамилия пользователя', validators=[DataRequired()])
+    city = StringField('Город')
+    country = StringField('Страна')
+    telephon_number = StringField('Телефон')
     about = TextAreaField("Немного о себе")
     submit = SubmitField('Продолжить')
 
