396f528f0bf06bd78770d9764c14eeebc0642b64
a-romash <romashov.artemiy@gmail.com>
2023-04-13 16:40:44 +0300

    added 1

--- main.py
+++ main.py
@@ -1,43 +1,196 @@
-import requests
-from pprint import pprint
+from flask import Flask, render_template, redirect, request, abort
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+import shutil
+
+import os
+
+from forms.news import NewsForm
+from forms.user import RegisterForm, LoginForm
+from data.news import News
+from data.users import User
+from data import db_session
+
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-city = input('Введите название населенного пункта ')
-geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={city}" \
-                   f"&format=json"
-
-response = requests.get(geocoder_request)
-if response:
-    json_response = response.json()
-    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-    toponym_coodrinates = toponym["Point"]["pos"]
-    lats = toponym_coodrinates.split()[0]
-    lons = toponym_coodrinates.split()[1]
-else:
-    print("Ошибка выполнения запроса:")
-    print(geocoder_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
-url_yandex = f'https://api.weather.yandex.ru/v2/forecast/?lat={lons}&lon={lats}&[lang=ru_RU]'
-yandex_req = requests.get(url_yandex, headers={'X-Yandex-API-Key': 'b5abe2c2-418a-405e-9cce-d1e7d1a2505a'})
-weather = yandex_req.json()
-prec = ['без осадков', 'дождь', 'дождь со снегом', 'снег', 'град']
-print(f"Город: {weather['info']['url']}")
-print(f"Температура: {weather['fact']['temp']}°C")
-print(f"Ощущаемая температура: {weather['fact']['feels_like']}°C")
-print(f"Скорость ветра: {weather['fact']['wind_speed']}м/с")
-print(f"Скорость порывов ветра: {weather['fact']['wind_gust']}м/с")
-print(f"Влажность: {weather['fact']['humidity']}%")
-print(f"Давление: {weather['fact']['pressure_mm']} мм рт. ст.")
-if weather['fact']['prec_strength'] == 0:
-    print('Осадки: Без осадков')
-elif weather['fact']['prec_strength'] == 0.25:
-    print(f"Осадки: Слабый {prec[weather['fact']['prec_type']]}")
-elif weather['fact']['prec_strength'] == 0.5:
-    print(f"Осадки: {prec[weather['fact']['prec_type']]}")
-elif weather['fact']['prec_strength'] == 0.75:
-    print(f"Осадки: Сильный {prec[weather['fact']['prec_type']]}")
-elif weather['fact']['prec_strength'] == 1:
-    print(f"Осадки: Очень сильный {prec[weather['fact']['prec_type']]}")
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
+
+def main():
+    db_session.global_init("db/maps.db")
+    app.run()
+
+
+@app.route('/news', methods=['GET', 'POST'])
+@login_required
+def add_news():
+    form = NewsForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = News()
+        news.title = form.title.data
+        news.content = form.content.data
+
+        f = form.map.data
+        print(f)
+        filename = secure_filename(f.filename)
+        i = 1
+        while f'{i}_{news.title}_{filename}' in os.listdir('static/img/news_img/'):
+            i += 1
+
+        f.save(os.path.join(app.instance_path, 'photos', f'{i}_{news.title}_{filename}'))
+
+
+        shutil.move(f'instance/photos/{i}_{news.title}_{filename}', f'static/img/news_img/{i}_{news.title}_{filename}')
+        news.map = f'{i}_{news.title}_{filename}'
+
+        news.is_private = form.is_private.data
+        current_user.news.append(news)
+        db_sess.merge(current_user)
+        db_sess.commit()
+
+        return redirect('/')
+    return render_template('news.html', title='Добавление новости', form=form)
+
+
+@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def news_delete(id):
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+    if news:
+        os.remove(f'static/img/news_img/{news.map}')
+        db_sess.delete(news)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/')
+
+
+@app.route('/news/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_news(id):
+    form = NewsForm()
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+        if news:
+            form.title.data = news.title
+            form.content.data = news.content
+
+            form.is_private.data = news.is_private
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+        if news:
+            news.title = form.title.data
+            news.content = form.content.data
+            news.is_private = form.is_private.data
+            db_sess.commit()
+            return redirect('/')
+        else:
+            abort(404)
+    return render_template('news.html', title='Редактирование новости', form=form)
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter((News.user == current_user) | (News.is_private != True))
+    else:
+        news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html', message="Неправильный логин или пароль", form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/profile_user/<int:id_user>', methods=['GET', 'POST'])
+def profile_user(id_user):
+    if current_user.id == id_user:
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.id == id_user).first()
+        news = db_sess.query(News).filter(News.user == current_user)
+        return render_template('profile_user.html', title='Профиль', user=user, news=news)
+
+
+@app.route('/profile_edit/<int:id_user>', methods=['GET', 'POST'])
+def profile_edit(id_user):
+    if current_user.id == id_user:
+        form = RegisterForm()
+        if request.method == "GET":
+            db_sess = db_session.create_session()
+            user = db_sess.query(User).filter(User.id == id_user).first()
+            if user:
+                form.name.data = user.name
+                form.about.data = user.about
+                form.email.data = user.email
+            else:
+                abort(404)
+        if request.method == "POST":
+            db_sess = db_session.create_session()
+            user = db_sess.query(User).filter(User.id == id_user).first()
+            if user:
+                user.name = form.name.data
+                user.about = form.about.data
+                user.email = form.email.data
+                db_sess.commit()
+                return redirect(f'/profile_user/{id_user}')
+            else:
+                abort(404)
+        return render_template('profile_edit.html', title='Изменение профиля', form=form)
+
+
+if __name__ == '__main__':
+    main()
